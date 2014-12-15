a31cd3d99ec1a4d776ed41deb6d3c546e9ba670d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2014-12-15 12:30:56 +0100

    Initial Commit

---
+++ CIKernelMKMapViewCrash/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CIKernelMKMapViewCrash
+//
+//  Created by Rolf Bjarne Kvinge on 15/12/14.
+//  Copyright (c) 2014 me. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
